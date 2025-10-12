8a2c64bd67093dfb4eb093d8ddd91ba2a2b6c007
Kishor Neupane <neupkishor.5@gmail.com>
2025-10-12 06:51:27 +0000

    change the font to raleway

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
+        body: ['Raleway', 'sans-serif'],
         headline: ['"Space Grotesk"', 'sans-serif'],
         code: ['"Fira Code"', 'monospace'],
       },
